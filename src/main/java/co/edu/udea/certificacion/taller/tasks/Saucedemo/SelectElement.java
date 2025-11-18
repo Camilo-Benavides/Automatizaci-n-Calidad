a9d5b2e198afc1c39ffb60703fb1056edccf570b
Ricardo Contreras Garzón <102176435+RickContreras@users.noreply.github.com>
2025-11-18 09:03:50 +0000

    feat: Refactor la clase SelectElement para agregar productos al carrito de compras de manera modular mejorando las capturas de imagenes

--- src/main/java/co/edu/udea/certificacion/taller/tasks/Saucedemo/SelectElement.java
+++ src/main/java/co/edu/udea/certificacion/taller/tasks/Saucedemo/SelectElement.java
@@ -1,24 +1,51 @@
 package co.edu.udea.certificacion.taller.tasks.Saucedemo;
 
 import co.edu.udea.certificacion.taller.utils.WaitTime;
+import net.serenitybdd.annotations.Step;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 import static co.edu.udea.certificacion.taller.userinterfaces.SaucedemoPage.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class SelectElement implements Task {
+
+    @Step("{0} selecciona y agrega 3 productos al carrito de compras")
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Click.on(ADD_ELEMENT_1));
-        WaitTime.putWaitTimeOf(2000);
-        actor.attemptsTo(Click.on(ADD_ELEMENT_2));
-        WaitTime.putWaitTimeOf(2000);
-        actor.attemptsTo(Click.on(ADD_ELEMENT_3));
+        addFirstProduct(actor);
+        addSecondProduct(actor);
+        addThirdProduct(actor);
     }
 
-    public static SelectElement elements(){
+    @Step("Usuario agrega el primer producto (Sauce Labs Backpack) al carrito")
+    private void addFirstProduct(Actor actor) {
+        actor.attemptsTo(
+                WaitUntil.the(ADD_ELEMENT_1, isVisible()).forNoMoreThan(10).seconds(),
+                Click.on(ADD_ELEMENT_1));
+        WaitTime.putWaitTimeOf(1500);
+    }
+
+    @Step("Usuario agrega el segundo producto (Sauce Labs Bike Light) al carrito")
+    private void addSecondProduct(Actor actor) {
+        actor.attemptsTo(
+                WaitUntil.the(ADD_ELEMENT_2, isVisible()).forNoMoreThan(10).seconds(),
+                Click.on(ADD_ELEMENT_2));
+        WaitTime.putWaitTimeOf(1500);
+    }
+
+    @Step("Usuario agrega el tercer producto (Sauce Labs Bolt T-Shirt) al carrito")
+    private void addThirdProduct(Actor actor) {
+        actor.attemptsTo(
+                WaitUntil.the(ADD_ELEMENT_3, isVisible()).forNoMoreThan(10).seconds(),
+                Click.on(ADD_ELEMENT_3));
+        WaitTime.putWaitTimeOf(1500);
+    }
+
+    public static SelectElement elements() {
         return Tasks.instrumented(SelectElement.class);
     }
 }
